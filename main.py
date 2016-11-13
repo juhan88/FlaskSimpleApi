596b0eeb8609de2a51defeb291f7bb27021b8c2a
tamabo88 <tamabo88@yahoo.ca>
2016-11-12 16:14:56 -0800

    Add POST request with JSON as body

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -12,8 +12,17 @@ class LoggedIn(Resource):
     def get(self):
         return {'loggedIn': True}
 
+class SubmitSession(Resource):
+    def post(self):
+        json_data = request.get_json(force=True)
+        user = json_data['username']
+        password = json_data['password']
+        return jsonify(u=user,p=password)
+
+
 api.add_resource(HelloWorld, '/')
 api.add_resource(LoggedIn, '/loggedIn')
+api.add_resource(SubmitSession, '/submitSession')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=9191, debug=True)
